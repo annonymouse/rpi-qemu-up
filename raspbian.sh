009c4b274f42334623977dad270d50e8af9a37ed
annonymouse <annonymouse@example.co.uk>
2014-12-30 01:08:30 +0000

    Make packer script less wonky, still doesn't work, also create startup script

--- raspbian.sh
+++ raspbian.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+set -e
 
 raspbian_zip=http://downloads.raspberrypi.org/raspbian/images/raspbian-2014-12-25/2014-12-24-wheezy-raspbian.zip
 qemu_kernel=http://xecdesign.com/downloads/linux-qemu/kernel-qemu
 
-wget $qemu_kernel
+mkdir -p resources
+wget $qemu_kernel -O resources/qemu-kernel
+wget $raspbian_zip -O resources/raspbian.zip
+pushd resources
+unzip raspbian.zip
+qemu-img convert -f raw -O qcow2 *.img raspbian.qcow2
+popd
 
 packer build raspbian.json
